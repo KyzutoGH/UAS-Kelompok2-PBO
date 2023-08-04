db2f7e3e24476e1321e890a23dbd8726f9414f0f
Muhammad Ikhsan <133367185+KyzutoGH@users.noreply.github.com>
2023-08-04 17:56:31 +0700

    Menambahkan penjelasan singkat

--- src/ProjectUas/PenjumlahanPengurangan.java
+++ src/ProjectUas/PenjumlahanPengurangan.java
@@ -10,16 +10,20 @@ package ProjectUas;
  * @author Asshyffatul Aina Ni'mah
  * 22104410044
  */
+
+//Import package untuk method scanner
 import java.util.Scanner;
 public class PenjumlahanPengurangan {
     public static void main(String[] args) {
+        //Deklarasi variabel
         int a = 100;
         int b = 25;
         int hasil = 0;
-         
+
+        //Inisiasi scanner agar bisa memasukkan inputan dari keyboard
          Scanner scanner = new Scanner(System.in);  {
             int pilihan;
-            
+            //Inisiasi perulangan
             do {
                 // Tampilkan menu pilihan
                 System.out.println("Menu Pilihan:");
@@ -29,25 +33,26 @@ public class PenjumlahanPengurangan {
                 
                 // Input pilihan pengguna
                 System.out.print("Masukkan pilihan Anda: ");
-                pilihan = scanner.nextInt();
-                
+                pilihan = scanner.nextInt();//scanner hanya bisa menerima inputan angka karena menggunakan method nextInt(); dan disimpan di variabel pilihan
+
+                //kondisi percabangan switch case yang diambil dari inputan "pilihan"
                 switch (pilihan) {
                     case 1:
                         hasil = a + b;
-                        System.out.println("Hasil penjumlahan: " + hasil);
+                        System.out.println("Hasil penjumlahan: " + hasil); //output dari penjumlahan dari angka yang sudah ditetapkan sebelumnya
                         break;
                     case 2:
                         hasil = a - b;
-                        System.out.println("Hasil pengurangan: " + hasil);
+                        System.out.println("Hasil pengurangan: " + hasil); //output dari penjumlahan dari angka yang sudah ditetapkan sebelumnya
                         break;
                     case 0:
-                        System.out.println("Terima kasih, program selesai.");
+                        System.out.println("Terima kasih, program selesai."); //memberikan pesan bahwa program akan berakhir
                         break;
                     default:
-                        System.out.println("Pilihan Anda salah. Silakan coba lagi.");
+                        System.out.println("Pilihan Anda salah. Silakan coba lagi."); //jika pengguna memilih menu yang tidak sesuai dengan tampilan di layar
                         break;
                 }
-            } while (pilihan != 0);
+            } while (pilihan != 0); //Jika pengguna memilih menu 0 maka program akan berakhir
         }
     }
 }
